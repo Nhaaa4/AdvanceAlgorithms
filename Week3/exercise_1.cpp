7a52823cff782bbcca045ca995125dd652a96a6c
Nhaaa4 <panahsovan51@gmail.com>
2024-10-23 12:11:32 +0700

    update

--- Week3/exercise_1.cpp
+++ Week3/exercise_1.cpp
@@ -1,8 +1,14 @@
 #include <iostream>
 #include <vector>
-
+using namespace std;
 bool isSorted(std::vector<int>& array) {
     //your code here
+    for (int i = 1; i < array.size(); i++) {
+        if (array[i - 1] > array[i]) {
+            return false;
+        }
+    }
+    return true;
 }
 
 int main() {
